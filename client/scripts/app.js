fe6befd11a5241951c62722451b83787e0c72f8a
lensvelt <lensvelt.brett@gmail.com>
2016-05-02 15:43:22 -0700

    Completed chatroom behaviors

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,34 +2,22 @@
 
 let app = {
 
-  parseAPI: 'https://api.parse.com/1/classes/messages',
+  server: 'https://api.parse.com/1/classes/messages',
 
   init: () => {
     
 
   },
 
-  // send: (message) => {
-  //   let post = $.ajax({
-  //     url: app.parseAPI,
-  //     type: 'POST',
-  //     data: JSON.stringify(message),
-  //     contentType: 'application/json'
-  //   });
-
-  //   post.done((response) => {
-  //     console.log('hello');
-  //     console.log(response);
-  //   });
-
   send: (message) => {
     $.ajax({
-      url: app.parseAPI,
+      url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: (data) => {
         console.log('chatterbox: Message sent');
+        console.log('SUCCESS POST: ', data);
       },
       error: (data) => {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,7 +27,31 @@ let app = {
   },
 
   fetch: () => {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: (data) => {
+        console.log('chatterbox: messages received');
+        console.log('SUCCESS GET: ', data);
+      },
+      error: (data) => {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to retreive messages', data);
+      }
+    });
+  },
 
+  clearMessages: () => {
+    $('#chats').empty();
+  },
+
+  addMessage: (message) => {
+    $('#chats').append( '<p>' + message + '</p>' );
+  },
+
+  addRoom: (room) => {
+    $('#roomSelect').append( '<p>' + room + '</p>' );
   }
 
 };
